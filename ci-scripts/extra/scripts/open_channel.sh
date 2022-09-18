2e6698ca756769081fa322e42a9c68b700a9dc91
Yan Maya <r3mix.il@gmail.com>
2022-09-18 11:46:24 +0300

    modified open_channel script to implement default connection, and changed README with updates

--- ci-scripts/extra/scripts/open_channel.sh
+++ ci-scripts/extra/scripts/open_channel.sh
@@ -8,8 +8,8 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         -ac|--a-contract) a_contract="$2"; shift ;;
         -bc|--b-contract) b_contract="$2"; shift ;;
-        -c|--a-connection) connection="$2"; shift ;;
         -cv|--channel-version) version="$2"; shift ;;
+        -c|--a-connection) connection="$2"; shift ;;
         -o|--order) order="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
@@ -26,7 +26,13 @@ if [ -z "$b_contract" ]; then echo "[ERROR] Please provide contract address on o
 if [ -z "$version" ]; then echo "[ERROR] Please provide version of contracts" && exit 1; fi
 
 # If no connection we try connection-0
-if [ -z "$connection" ]; then connection="connection-0"; fi
+if [ -z "$connection" ]; then 
+    # get connection
+    CONNECTION=$(docker exec -it wasmd \
+    wasmd query ibc connection connections --output json | jq '.["connections"][0] | .["id"]' | tr -d '"')
+else
+    CONNECTION=$connection
+fi
 
 # If no order we set it unordered
 if [ -z "$order" ]; then order="unordered"; fi
@@ -38,10 +44,6 @@ wasmd query wasm contract $a_contract --output json | jq '.["contract_info"] | .
 # get b contract port
 CONTRACT_B_PORT=$(docker exec -it osmosis \
 osmosisd query wasm contract $b_contract --output json | jq '.["contract_info"] | .["ibc_port_id"]' | tr -d '"')
-
-# get connection
-CONNECTION=$(docker exec -it wasmd \
-wasmd query ibc connection connections --output json | jq '.["connections"][0] | .["id"]' | tr -d '"')
 
 $HERMES create channel --a-chain wasmd-1 --a-connection $CONNECTION \
 --a-port $CONTRACT_A_PORT \
