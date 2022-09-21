fc226e6cb2c94132126d69c8deb57cff20b1d9d2
Art3mix <r3mix.il@gmail.com>
2022-09-21 19:19:07 +0300

    Made the scripts a little nicer, and added the upload script

--- ci-scripts/extra/scripts/init.sh
+++ ci-scripts/extra/scripts/init.sh
@@ -2,8 +2,6 @@
 
 PASSWORD="1234567890"
 
-admin=1
-
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -d|--docker-name) docker="$2"; shift ;;
@@ -11,49 +9,55 @@ while [[ "$#" -gt 0 ]]; do
         -w|--wallet) wallet="$2"; shift ;;
         -i|--init-json) init_json="$2"; shift ;;
         -l|--label) label="$2"; shift ;;
-        -nd|--no-admin) admin=0; shift ;;
+        -nd|--no-admin) admin=1; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
 # Verify we got the docker, either wasmd or osmosis
-if [ -z "$docker" ]; then echo "[ERROR] Please include docker name [wasmd|osmosis]" && exit 1; fi
+if [ -z "$docker" ]; then echo "[ERROR] Please include docker name: -d [wasmd|osmosis]" && exit 1; fi
 
 # Verify we got the contract name to upload
-if [ -z "$code_id" ]; then echo "[ERROR] Must provide code id" && exit 1; fi
+if [ -z "$code_id" ]; then echo "[ERROR] Must provide code id: -c 1" && exit 1; fi
 
 # Set default label
 if [ -z "$label" ]; then label="testing"; fi
 
 # Verify we have a wallet, either a default wallet, or given by user
-if [ -z "$wallet" ]; then
-    if [[ "$docker" == *"osmo"* ]]; then
-        chain_client="osmosisd"
-        chain_id="osmo-testing"
-        wallet="osmo1ll3s59aawh0qydpz2q3xmqf6pwzmj24t9ch58c"
-    elif [[ "$docker" == *"wasm"* ]]; then
-        chain_client="wasmd"
-        chain_id="wasmd-1"
-        wallet="wasm1ll3s59aawh0qydpz2q3xmqf6pwzmj24t8l43cp"
-    else 
-        echo "[ERROR] Didn't recognized '$docker', please provide a wallet" && exit 1;
-    fi
+if [[ "$docker" == *"osmo"* ]]; then
+    chain_client="osmosisd"
+    chain_id="osmo-testing"
+    chain_coin="osmo"
+
+    if [ -z "$wallet" ]; then wallet="osmo1ll3s59aawh0qydpz2q3xmqf6pwzmj24t9ch58c"; fi
+elif [[ "$docker" == *"wasm"* ]]; then
+    chain_client="wasmd"
+    chain_id="wasmd-1"
+    chain_coin="cosm"
+
+    if [ -z "$wallet" ]; then wallet="wasm1ll3s59aawh0qydpz2q3xmqf6pwzmj24t8l43cp"; fi
+else 
+    echo "[ERROR] Didn't recognized '$docker', please provide a wallet: -w [WALLET]" && exit 1;
 fi
 
-if [ -n "$admin" ]; then 
+if [ -z "$admin" ]; then 
     admin="--admin $wallet"
 else 
     admin="--no-admin"
 fi
 
 # Verify we got the init message or set it to empty
-if [ -z "$init_json" ]; then init_json="{}"; fi
+if [ -z "$init_json" ]; then init_json='{}'; fi
 
 # Init the contract and get the contract address from it.
 CONTRACT_ADDR=$(echo "$PASSWORD" | docker exec -i $docker \
-$chain_client tx wasm init $code_id $init_json --from $wallet $admin \
+$chain_client tx wasm init $code_id "$init_json" --from $wallet $admin \
 --label $label --node http://127.0.0.1:26657 --chain-id $chain_id --gas-prices 0.1ucosm --gas auto --gas-adjustment 1.3 -b block -y --output json \
 | jq '.["logs"][0] | .["events"][0] | .["attributes"][0].value')
 
-echo $CONTRACT_ADDR
+echo $( jq -n \
+            --argjson code_id "$code_id" \
+            --arg docker "$docker" \
+            --argjson contract_addr "$CONTRACT_ADDR" \
+            '{chain: $docker, code_id: $code_id, contract_address: $contract_addr'} )
--- ci-scripts/extra/scripts/upload.sh
+++ ci-scripts/extra/scripts/upload.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-PASSWORD="1234567890"
+PASSWORD=1234567890
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -d|--docker-name) docker="$2"; shift ;;
         -c|--contract-name) contract="$2"; shift ;;
         -w|--wallet) wallet="$2"; shift ;;
-        -i|--init-json) init_json="$2"; shift ;;
-        -l|--label) label="$2"; shift ;;
-        -nd|--no-admin) admin=1; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
 # Verify we got the docker, either wasmd or osmosis
-if [ -z "$docker" ]; then echo "[ERROR] Please include docker name [wasmd|osmosis]" && exit 1; fi
+if [ -z "$docker" ]; then echo "[ERROR] Please include docker name: -d [wasmd|osmosis]" && exit 1; fi
 
 # Verify we got the contract name to upload
-if [ -z "$contract" ]; then echo "[ERROR] Please include contract name to upload [example.wasm]" && exit 1; fi
-
-# Set default label
-if [ -z "$label" ]; then label="testing"; fi
+if [ -z "$contract" ]; then echo "[ERROR] Please include contract name to upload: -c [example.wasm]" && exit 1; fi
 
 # Verify we have a wallet, either a default wallet, or given by user
 if [[ "$docker" == *"osmo"* ]]; then
@@ -41,26 +35,13 @@ else
     echo "[ERROR] Didn't recognized '$docker', please provide a wallet: -w [WALLET]" && exit 1;
 fi
 
-# Set --no-admin or the provided/default wallet
-if [ -z "$admin" ]; then 
-    admin="--admin $wallet"
-    is_admin=""
-else 
-    admin="--no-admin"
-    is_admin="-nd"
-fi
-
-# Verify we got the init message or set it to empty
-if [ -z "$init_json" ]; then init_json="{}"; fi
-
 # Upload the contract to the chain, and get the code id from it.
-CODE_ID=$(./upload.sh -d $docker -c $contract -w $wallet | jq '.["code_id"]')
-
-# Init the contract and get the contract address from it.
-CONTRACT_ADDR=$(./init.sh -d $docker -c $CODE_ID -w $wallet -i "$init_json" -l $label $is_admin | jq '.["contract_address"]')
+CODE_ID=$(echo "$PASSWORD" | docker exec -i $docker \
+$chain_client tx wasm store /template/contracts/$contract --from $wallet \
+--node http://127.0.0.1:26657 --chain-id $chain_id \
+--gas-prices 0.1u$chain_coin --gas auto --gas-adjustment 1.3 -b block -y --output json | jq '.["logs"][0] | .["events"][1] | .["attributes"][0].value | tonumber')
 
 echo $( jq -n \
             --argjson code_id "$CODE_ID" \
             --arg docker "$docker" \
-            --argjson contract_addr "$CONTRACT_ADDR" \
-            '{chain: $docker, code_id: $code_id, contract_address: $contract_addr'} )
+            '{chain: $docker, code_id: $code_id}' )
