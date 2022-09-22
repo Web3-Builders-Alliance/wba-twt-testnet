43c00576c1fb1c7fbabc92fe6d16cae3442b4895
Reece Williams <reecepbcups@gmail.com>
2022-09-22 10:56:06 -0500

    runs from dirname of script folder

--- hermes/start.sh
+++ hermes/start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Allows starting hermes from root dir of the project
+# cd "$(dirname "$0")"
+FOLDER="$(dirname "$0")"
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -c|--config) config="$2"; shift ;;
@@ -11,7 +15,7 @@ done
 # Verify we got the docker, either wasmd or osmosis
 if [ -z "$config" ]; then config="config.toml"; fi
 
-HERMES="hermes --config $config"
+HERMES="hermes --config $FOLDER/$config"
 
 # First, lets make sure our chains are running and are healthy.
 # Exit if theres an error.
@@ -25,10 +29,10 @@ HERMES="hermes --config $config"
 # Because in hermes config, we set who is the payer wallet by the name of the key.
 
 # Create key in hermes for wasmd-1 chain with the name relayer1
-$HERMES keys add --chain wasmd-1 --key-name relayer1 --mnemonic-file relayer-mnemonic || true
+$HERMES keys add --chain wasmd-1 --key-name relayer1 --mnemonic-file $FOLDER/relayer-mnemonic || true
 
 # Create key in hermes for osmo-testing chain with the name relayer2
-$HERMES keys add --chain osmo-testing --key-name relayer2 --mnemonic-file relayer-mnemonic || true
+$HERMES keys add --chain osmo-testing --key-name relayer2 --mnemonic-file $FOLDER/relayer-mnemonic || true
 
 # Lets start a connection between our 2 chains, make sure the chains are running or else this will not work.
 $HERMES create connection --a-chain wasmd-1 --b-chain osmo-testing;
